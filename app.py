b48bfe12dae91e084976770b577e21da85f0b1bc
Nang mon kham <103887616+monkham21@users.noreply.github.com>
2025-04-20 13:24:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.5, 3.5)
 petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 1.4)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.2)
 
+scaler = StandardScaler()
+
 #Predict button
 if st.button("Predict"):
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
