2a2115d821dc3b724b1e30d7539c936af483278c
Nang mon kham <103887616+monkham21@users.noreply.github.com>
2025-04-20 13:30:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,59 @@
-#import library
+# #import library
+# import streamlit as st
+# import numpy as np
+# from sklearn.preprocessing import StandardScaler
+# import pickle
+
+# #load model
+# with open('dtm_trained_model.pkl', 'rb') as f:
+#     dtm_model = pickle.load(f)
+    
+# #application title
+# st.title("Iris Flower Classification")
+# st.write("Enter the features of the iris flower: ")
+
+# #Input fields
+# sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0, 5.1)
+# sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.5, 3.5)
+# petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 1.4)
+# petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.2)
+
+# #Predict button
+# if st.button("Predict"):
+#     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+#     prediction = dtm_model.predict(input_data)
+#     species = ['Setosa', 'Versicolor', 'Virginica']
+#     st.success(f"The prediced species is: **{species[prediction[0]]}**")
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 18 23:15:47 2025
+
+@author: Nongnuch
+"""
+
+# dtm_app.py
 import streamlit as st
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import pickle
 
-#load model
+# Load model
 with open('dtm_trained_model.pkl', 'rb') as f:
     dtm_model = pickle.load(f)
-    
-#application title
-st.title("Iris Flower Classification")
-st.write("Enter the features of the iris flower: ")
 
-#Input fields
+# App title
+st.title("🌼 Iris Flower Classification")
+st.write("Enter the features of the iris flower:")
+
+# Input fields
 sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0, 5.1)
 sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.5, 3.5)
 petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 1.4)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.2)
 
-#Predict button
+# Predict button
 if st.button("Predict"):
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = dtm_model.predict(input_data)
     species = ['Setosa', 'Versicolor', 'Virginica']
-    st.success(f"The prediced species is: **{species[prediction[0]]}**")
+    st.success(f"The predicted species is: **{species[prediction[0]]}**")
